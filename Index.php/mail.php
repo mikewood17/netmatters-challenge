c43f563d5a9551c590c8e59418b618da34ce28cd
mikewood17 <113477758+mikewood17@users.noreply.github.com>
2022-09-15 23:20:11 +0100

    Add files via upload

--- Index.php/mail.php
+++ Index.php/mail.php
@@ -1,21 +1,33 @@
 <?php
-  $firstName = $_POST['firstName'];
+
+  $firstName = $_POST['firstName']; 
   $lastName = $_POST['lastName'];
   $email = $_POST['email'];
   $phoneNumber = $_POST['phoneNumber'];
   $subject = $_POST['subject'];
   $message = $_POST['message'];
-
   //Database connection
+     $conn = new mysqli('localhost' , 'root' , '' , 'phpcontactform');
 
-  $conn = new mysqli('localhost' , 'root' , '' , 'phpcontactform');
-  if($conn->connect_error){
+
+     if($conn->connect_error){
       die('Connection Failed : '.$conn->connect_error);
-  } else {
-      $stmt = $conn->prepare("insert into registration(firstName, lastName, email, phoneNumber, subject, message) value(?, ?, ?, ?, ?, ?)");
-      $stmt->bind_param("sssssi",$firstName, $lastName, $email, $phoneNumber, $subject, $message);
-      $stmt->execute();
-      echo "registration successful"}
+    } else {
+    
+    $insertQuery = "insert into contactformdata (firstName, lastName, email, phoneNumber, subject, message) values('". $firstName ."', '". $lastName ."', '". $email ."', '". $phoneNumber ."', '". $subject ."', '". $message ."')";
+      
+try{ 
+      
+    
+      if ($conn->query($insertQuery)
+        == TRUE){echo "registration successful";} else {
+            echo "Something went wrong, please try again";
+        }
+    }catch (Exception $e){
+          echo $e->getMessage();
+      }
+      $conn->close();
+    }
         
 ?>
 
