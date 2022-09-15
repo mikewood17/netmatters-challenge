c9dfbf7de8a5bb4fbf6b2da4ec4095787d5f7af9
mikewood17 <113477758+mikewood17@users.noreply.github.com>
2022-09-15 16:26:02 +0100

    Add files via upload

--- Index.php/mail.php
+++ Index.php/mail.php
@@ -1,22 +1,23 @@
 <?php
+  $firstName = $_POST['firstName'];
+  $lastName = $_POST['lastName'];
+  $email = $_POST['email'];
+  $phoneNumber = $_POST['phoneNumber'];
+  $subject = $_POST['subject'];
+  $message = $_POST['message'];
 
-if(isset($_POST["submit"])) {
-    $firstname = $_POST["firstname"];
-    $lastname = $_POST["lastname"];
-    $email = $_POST["email"];
-    $telephoneNumber = $_POST["telephoneNumber"];
-    $subject = $_POST["subject"];
-    $message = $_POST["message"];
+  //Database connection
 
-    $mailTo = "georgiagoodall1@hotmail.com";
-    $headers = "From: ".$email;
-    $txt = "You have received an email from ".$firstname.".\n\n".$message;
-
-
-    mail($mailTo, $subject, $txt, $headers);
-
-    header("Location: index.php?mailsend");
-}
+  $conn = new mysqli('localhost' , 'root' , '' , 'phpcontactform');
+  if($conn->connect_error){
+      die('Connection Failed : '.$conn->connect_error);
+  } else {
+      $stmt = $conn->prepare("insert into registration(firstName, lastName, email, phoneNumber, subject, message) value(?, ?, ?, ?, ?, ?)");
+      $stmt->bind_param("sssssi",$firstName, $lastName, $email, $phoneNumber, $subject, $message);
+      $stmt->execute();
+      echo "registration successful"}
+        
+?>
 
 
 
