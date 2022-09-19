6013976e7c67733d8f7ff83eee5053258a30c297
mikewood17 <113477758+mikewood17@users.noreply.github.com>
2022-09-19 18:17:48 +0100

    Add files via upload

--- Index.php/mail.php
+++ Index.php/mail.php
@@ -6,16 +6,26 @@
   $phoneNumber = $_POST['phoneNumber'];
   $subject = $_POST['subject'];
   $message = $_POST['message'];
+
+        
   //Database connection
      $conn = new mysqli('localhost' , 'root' , '' , 'phpcontactform');
 
+  $regex = "/^[a-zA-Z\d\.]+@[a-zA-Z\d\.]+\.[a-zA-Z\d\.]{2,}+$/";
+  if(isset($_POST['submit'])) {
+  if(preg_match($regex, $_POST['email']))  {
+    $output = "<span style='color:green'>Valid Email</span>";
+  } else {
+    $output = "<span style='color:red'>Invalid Email</span>";
+    }
+  }
 
      if($conn->connect_error){
       die('Connection Failed : '.$conn->connect_error);
     } else {
     
     $insertQuery = "insert into contactformdata (firstName, lastName, email, phoneNumber, subject, message) values('". $firstName ."', '". $lastName ."', '". $email ."', '". $phoneNumber ."', '". $subject ."', '". $message ."')";
-      
+
 try{ 
       
     
